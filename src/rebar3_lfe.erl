153eaf199ffd6aa31cfa3be23fc53758a4d0dde2
Duncan McGreggor <oubiwann@gmail.com>
2020-07-13 14:39:07 -0500

    First try at basic REPL support.

--- src/rebar3_lfe.erl
+++ src/rebar3_lfe.erl
@@ -5,7 +5,8 @@
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
   Commands = [ fun rebar3_lfe_prv_compile:init/1,
-               fun rebar3_lfe_prv_run:init/1
+               fun rebar3_lfe_prv_repl:init/1,
+              fun rebar3_lfe_prv_run:init/1
              ],
   FoldFun  = fun(F, {ok, StateAcc}) -> F(StateAcc) end,
   lists:foldl(FoldFun, {ok, State}, Commands).
---
+++ src/rebar3_lfe_prv_repl.erl
@@ -0,0 +1,75 @@
+-module(rebar3_lfe_prv_repl).
+
+-export([init/1,
+         do/1,
+         format_error/1]).
+
+-define(PROVIDER, repl).
+-define(NAMESPACE, lfe).
+-define(DEPS, [compile]).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    State1 = rebar_state:add_provider(
+            State,
+            providers:create([
+                {namespace, ?NAMESPACE},
+                {name, ?PROVIDER},
+                {module, ?MODULE},
+                {bare, true},
+                {deps, ?DEPS},
+                {example, "rebar3 lfe repl"},
+                {short_desc, "Run an LFE REPL with project apps and deps in path."},
+                {desc, info()},
+                {opts, [{name, undefined, "name", atom,
+                         "Gives a long name to the node."},
+                        {sname, undefined, "sname", atom,
+                         "Gives a short name to the node."},
+                        {setcookie, undefined, "setcookie", atom,
+                         "Sets the cookie if the node is distributed."},
+                        {apps, undefined, "apps", string,
+                         "A list of apps to boot before starting the "
+                         "shell. (E.g. --apps app1,app2,app3) Defaults "
+                         "to rebar.config {shell, [{apps, Apps}]} or "
+                         "relx apps if not specified."},
+                        {relname, $r, "relname", atom,
+                         "Name of the release to use as a template for the "
+                         "shell session"},
+                        {relvsn, $v, "relvsn", string,
+                         "Version of the release to use for the shell "
+                         "session"},
+                        {start_clean, undefined, "start-clean", boolean,
+                         "Cancel any applications in the 'apps' list "
+                         "or release."},
+                        {env_file, undefined, "env-file", string,
+                         "Path to file of os environment variables to setup "
+                         "before expanding vars in config files."}]}
+            ])
+    ),
+    {ok, State1}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(Config) ->
+    rebar_api:debug("\tStarting LFE REPL ...", []),
+    rebar_api:debug("\t\tSetting shell args ...", []),
+    Config1 = rebar_state:set(Config, shell, [{shell_args, ['tty_sl -c -e',{lfe_shell,start,[]}]}]),
+    rebar_api:debug("\t\tCalling underlying rebar3 shell 'do' function ...", []),
+    rebar_prv_shell:do(Config1),
+    {ok, Config1}.
+
+-spec format_error(any()) -> iolist().
+format_error({unknown_app, Unknown}) ->
+    io_lib:format("Applications list for repl contains an unrecognizable application definition: ~p", [Unknown]);
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
+%% =============================================================================
+%% Internal functions
+%% =============================================================================
+
+info() ->
+    "Start an LFE REPL with project and deps preloaded similar to~n'lfe -pa ebin -pa deps/*/ebin'.~n".
