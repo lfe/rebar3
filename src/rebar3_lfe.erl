dc65d3f4bd73f8bed0e38c3b594182159dc1bf1b
Duncan McGreggor <oubiwann@gmail.com>
2020-07-15 12:27:53 -0500

    Added support for building releases for LFE projects.

--- src/rebar3_lfe.erl
+++ src/rebar3_lfe.erl
@@ -6,6 +6,7 @@
 init(State) ->
   Commands = [ fun rebar3_lfe_prv_compile:init/1,
                fun rebar3_lfe_prv_escriptize:init/1,
+               fun rebar3_lfe_prv_release:init/1,
                fun rebar3_lfe_prv_repl:init/1,
                fun rebar3_lfe_prv_run:init/1,
                fun rebar3_lfe_prv_run_escript:init/1
