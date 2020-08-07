748291571e2c8bc3787219988daa768675ed42d8
Duncan McGreggor <oubiwann@gmail.com>
2020-08-06 20:24:22 -0500

    Added package support debugging.

--- src/rebar3_lfe_package.erl
+++ src/rebar3_lfe_package.erl
@@ -1,5 +1,5 @@
 %%% This module contains the logic for working with faux packages in LFE.
-%%% 
+%%%
 %%% Essentially, this boils down to creating modules with dotted-notation
 %%% prefixes in their names. This can be done in the same `src` directory
 %%% as regular Erlang and LFE projects, or, if a developer prefers, it can
@@ -8,10 +8,10 @@
 %%% directory and the file name itself. To be clear, in the end what is
 %%% created is simply a module name, as there is no such thing in Erlang as
 %%% a package.
-%%% 
+%%%
 %%% Some examples of how files and directories will be transformed in their
 %%% final form as `.beam` files:
-%%% 
+%%%
 %%% ./src/my.package1.lfe             -> ebin/my.package1.beam
 %%% ./src/my/package2.lfe             -> ebin/my.package2.beam
 %%% ./src/my/other/package.lfe        -> ebin/my.other.package.beam
@@ -19,11 +19,11 @@
 %%% ./src/my/really/deep.package2.lfe -> ebin/my.really.deep.package2.beam
 %%% ./src/my/really.deep.package3.lfe -> ebin/my.really.deep.package3.beam
 %%% ./src/my.really.deep.package4.lfe -> ebin/my.really.deep.package4.beam
-%%% 
+%%%
 %%% So, in essence, this module is responsible for transforming a nested
 %%% directory structure into a single file, as well as providing the functions
 %%% necessary to perform any cleanup of intermediary, temporary files.
-%%% 
+%%%
 -module(rebar3_lfe_package).
 
 -export([generate_sources/1,
@@ -39,12 +39,14 @@
 -define(DEFAULT_FILE_LIST, []).
 
 generate_sources(SourceDirs) ->
-    [generate_source(Path) || Path <- SourceDirs].
+    AllFiles = [generate_source(Path) || Path <- SourceDirs],
+    rebar_api:debug("All package files: ~p", [AllFiles]).
 
 generate_source(Path) ->
     Files = files(filename:absname(Path)),
     rebar_api:debug("Package files for source dir ~p: ~p", [Path, Files]),
-    copy(Files).
+    copy(Files),
+    Files.
 
 package_to_source(SourcePath, Filepath) ->
     rebar_api:debug("SourcePath, Filepath: ~p, ~p", [SourcePath, Filepath]),
@@ -55,7 +57,7 @@ package_to_source(SourcePath, Filepath) ->
     filename:join([SourcePath, Replaced]).
 
 files(SourcePath) ->
-    %% This returns a list of maps for file names, where each map has 
+    %% This returns a list of maps for file names, where each map has
     %% 'package_file' and 'source_file' keys. The 'source_file' is the
     %% "flattened" module file copied from the 'package_file' (found in a
     %% subdirectory) where the filename has been assembled from the full path
@@ -65,7 +67,7 @@ files(SourcePath) ->
     AllFiles = filelib:fold_files(SourcePath, ?FILE_REGEX, ?RECURSE, AddFile, ?DEFAULT_FILE_LIST),
     PackageFiles = rebar3_lfe_utils:set_diff(AllFiles, ModsOnly),
     FileData = fun(Filepath) -> #{source_path => SourcePath,
-                                  package_file => Filepath, 
+                                  package_file => Filepath,
                                   source_file => package_to_source(SourcePath, Filepath)} end,
     PackageFileData = fun(Filepath, Acc) -> lists:append(Acc, [FileData(Filepath)]) end,
     lists:foldl(PackageFileData, ?DEFAULT_FILE_LIST, PackageFiles).
